a5bbc40b8fc6f9d93b5e2221ed8f1d0baeea0da0
PavlushaAnd <pavel.androsov.00@gmail.com>
2024-02-28 13:39:15 +1300

    database connection

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"scheduler_api/models"
 	_ "scheduler_api/routers"
 
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/beego/beego/v2/client/orm"
 	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/filter/cors"
 )
 
 func main() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", "root:WhisperingW@ves22@/schedulerdb?charset=utf8")
+	orm.RegisterModel(new(models.Task))
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"http://localhost:3000/"},
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET "},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
