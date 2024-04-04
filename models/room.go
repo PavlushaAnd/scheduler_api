7bd83c59de7874b5b825d4d8bddb5e21e235f167
PavlushaAnd <pavel.androsov.00@gmail.com>
2024-04-04 13:59:34 +1300

    room model;

---
+++ models/room.go
@@ -0,0 +1,63 @@
+package models
+
+import (
+	"time"
+
+	"github.com/beego/beego/v2/client/orm"
+)
+
+type Room struct {
+	Name         string    `orm:"column(name)"`
+	Sequence     int       `orm:"column(sequence)"`
+	LastModified time.Time `orm:"column(last_modified)"`
+	Version      int       `orm:"version"`
+}
+
+func init() {
+	orm.RegisterModel(new(Room))
+}
+
+func InsertRoom(room *Room, o orm.Ormer) error {
+	_, err := o.Insert(room)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UpdateRoom(room *Room, o orm.Ormer) error {
+	_, err := o.Update(room)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func DeleteRoom(room *Room, o orm.Ormer) error {
+	_, err := o.Delete(room)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ListRoom(roomName string, o orm.Ormer) ([]*Room, error) {
+	var rooms []*Room
+	qs := o.QueryTable(new(Room))
+
+	if roomName != "" {
+		qs = qs.Filter("name__icontains", roomName)
+	}
+
+	_, err := qs.All(rooms)
+	if err != nil {
+		if err == orm.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return rooms, nil
+}
